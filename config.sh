29f9ec720e8b84d04d2b2b0daea008978368854d
snoopy86 <simon@medved.in>
2015-07-23 11:36:06 +0200

    new

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 CONFIG="/folders2flickr"
 
 if [[ ! -f $CONFIG/.uploadr.ini ]]; then
-	cp root/.local/share/folders2flickr/uploadr.ini.sample $CONFIG/.uploadr.ini
+	cp /root/.local/share/folders2flickr/uploadr.ini.sample $CONFIG/.uploadr.ini
 else
-	if [[ $CONFIG/.uploadr.ini -nt root/.uploadr.ini ]]; then
-		cp $CONFIG/.uploadr.ini root/.uploadr.ini
+	if [[ $CONFIG/.uploadr.ini -nt /root/.uploadr.ini ]]; then
+		cp $CONFIG/.uploadr.ini /root/.uploadr.ini
 	fi
 fi
