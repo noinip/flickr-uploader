86cfbceb24147b2d73d5d5ae890076d4401f724b
snoopy86 <simon@medved.in>
2015-07-24 12:58:33 +0200

    details details

--- config.sh
+++ config.sh
@@ -2,10 +2,10 @@
 CONFIG="/folders2flickr"
 INIT="/etc/my_init.d"
 
-if [[ ! -f $CONFIG/.uploadr.ini ]]; then
-	cp $INIT/uploadr.ini $CONFIG/.uploadr.ini
+if [[ ! -f $CONFIG/uploadr.ini ]]; then
+	cp $INIT/uploadr.ini $CONFIG/uploadr.ini
 else
-	if [[ $CONFIG/.uploadr.ini -nt $INIT/.uploadr.ini ]]; then
-		cp $CONFIG/.uploadr.ini $INIT/.uploadr.ini
+	if [[ $CONFIG/uploadr.ini -nt $INIT/uploadr.ini ]]; then
+		cp $CONFIG/uploadr.ini $INIT/uploadr.ini
 	fi
 fi
